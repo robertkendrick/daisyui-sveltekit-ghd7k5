8b925d43fc54adf38238ce7d5267bb94f9f22273
robertkendrick <bobkendrick@blueyonder.co.uk>
2023-10-01 15:11:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# daisyui-sveltekit-ghd7k5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/daisyui-sveltekit-ghd7k5)
---
+++ src/routes/+page.svelte
@@ -0,0 +1,79 @@
+<button class="btn">Hello daisyUI</button>
+<!-- <div class="navbar bg-base-100"> -->
+<div class="navbar bg-slate-300">
+  <div class="flex-1">
+    <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+  </div>
+  <div class="flex-none">
+    <ul class="menu menu-horizontal px-1">
+      <li><a>Link</a></li>
+      <li>
+        <details>
+          <summary>
+            Parent
+          </summary>
+          <ul class="p-2 bg-base-100">
+            <li><a>Link 1</a></li>
+            <li><a>Link 2</a></li>
+          </ul>
+        </details>
+      </li>
+    </ul>
+  </div>
+</div>
+
+<div class="card w-96 bg-base-100 shadow-xl">
+  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+
+<div class="flex w-full">
+  <div class="grid h-20 flex-grow bg-base-100 mt-2">
+    lorem-40 is this going all the way to the edge
+  </div>
+  <div class="divider divider-horizontal"></div>
+  <div class="grid h-20 flex-grow bg-base-100 mt-2">
+    lorem-40 is this going all the way to the edge</div>
+</div>
+
+
+<footer class="footer p-10 bg-base-200 text-base-content">
+  <nav>
+    <header class="footer-title">Services</header> 
+    <a class="link link-hover">Branding</a> 
+    <a class="link link-hover">Design</a> 
+    <a class="link link-hover">Marketing</a> 
+    <a class="link link-hover">Advertisement</a>
+  </nav> 
+  <nav>
+    <header class="footer-title">Company</header> 
+    <a class="link link-hover">About us</a> 
+    <a class="link link-hover">Contact</a> 
+    <a class="link link-hover">Jobs</a> 
+    <a class="link link-hover">Press kit</a>
+  </nav> 
+  <nav>
+    <header class="footer-title">Legal</header> 
+    <a class="link link-hover">Terms of use</a> 
+    <a class="link link-hover">Privacy policy</a> 
+    <a class="link link-hover">Cookie policy</a>
+  </nav> 
+  <form>
+    <header class="footer-title">Newsletter</header> 
+    <fieldset class="form-control w-80">
+      <label class="label">
+        <span class="label-text">Enter your email address</span>
+      </label> 
+      <div class="relative">
+        <input type="text" placeholder="username@site.com" class="input input-bordered w-full pr-16" /> 
+        <button class="btn btn-primary absolute top-0 right-0 rounded-l-none">Subscribe</button>
+      </div>
+    </fieldset>
+  </form>
+</footer>
